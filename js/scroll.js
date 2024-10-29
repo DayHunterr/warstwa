f6bead9499725f2be0c37ff712ca302ee4c733aa
VladLevchenko <clernovmail@gmail.com>
2024-10-29 23:33:03 +0200

    change some script

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,13 @@
 
-$(function() {
-    $('#snap-container').pageScroller({
-        // Set configuration options here
-        travelTime: 1000,           // Animation speed in milliseconds
-        afterTravelTimeout: 0,       // Delay in seconds
-        travelEasing: 'swing',       // Easing function
-
-    });
-});
+// $(function() {
+//     $('#snap-container').pageScroller({
+//         // Set configuration options here
+//         travelTime: 1000,           // Animation speed in milliseconds
+//         afterTravelTimeout: 0,       // Delay in seconds
+//         travelEasing: 'swing',       // Easing function
+//
+//     });
+// });
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
